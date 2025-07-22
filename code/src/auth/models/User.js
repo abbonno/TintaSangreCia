1b139923a757406a5aba3318da0f0c995bb4ef91
ibradev <ibraadev@gmail.com>
2025-07-23 00:49:53 +0200

    Inicio de Inicio de sesion xd

---
+++ code/src/auth/models/User.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
+  // ...otros campos
+});
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
