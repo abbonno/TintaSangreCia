b03416360d7cc803e24b556e2264b8783829b443
luis-valdivieso <luisestebanvaldiviesogonzalez@gmail.com>
2025-08-04 17:05:49 +0200

    Revert "Database basic schemas"
    This reverts commit d1cd9637b461095eef515145569416e1b5be2b7f.

--- code/src/auth/models/User.js
+++ code/src/auth/models/User.js
@@ -4,13 +4,7 @@ import bcrypt from 'bcryptjs';
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, select: false },
-  city: { type: String, required: true },
-  country: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  // activationCode: { type: String, required: true }, // maybe used later for book activation
-  isActive: { type: Boolean, default: true }, // cambiar a true cuando se tenga que activar el libro
-
+  // ...otros campos
 });
 
 UserSchema.pre('save', async function(next) {
